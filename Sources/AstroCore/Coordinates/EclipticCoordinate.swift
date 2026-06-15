67dae002377fcdfdc42769ebd19a9a47486154e4
Babywbx <5620530+babywbx@users.noreply.github.com>
2026-06-14 20:08:47 -0400

    📝 docs(coordinates): clarify heliocentric frame on eclipticcoordinate.distance

--- Sources/AstroCore/Coordinates/EclipticCoordinate.swift
+++ Sources/AstroCore/Coordinates/EclipticCoordinate.swift
@@ -3,7 +3,7 @@ public struct EclipticCoordinate: Sendable, Hashable, Codable {
     public let longitude: Double
     /// Ecliptic latitude in degrees [-90, 90]
     public let latitude: Double
-    /// Radial distance in AU (frame implied by the producing API)
+    /// Radial distance in AU; heliocentric when produced by `heliocentric(of:at:)`
     public let distance: Double
 
     public init(longitude: Double, latitude: Double, distance: Double) {
